d4286edc38a5b8a0e8d8b3f63107b9e56aa7d4e9
sone92cn <554055459@qq.com>
2018-03-01 09:03:14 +0800

    script

---
+++ push_image.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 18:35:11 2018
+
+@author: Felix
+"""
+
+import os
+from git import Repo
+
+path = os.path.split(os.path.realpath(__file__))[0]
+print("Current Path :", path)
+try:
+    repo = Repo(path)
+except:
+    print("Fail to create Repo.")
+else:
+    index = repo.index
+    try:
+        index.add(['img'])
+        index.commit('refresh style')
+    except:
+        print("Fail to refresh Repo.")
+    else:
+        remote = repo.remote()
+        try:
+            remote.push()
+        except:
+            print("Fail to push Repo.")
+        else:
+            print("Succeed to refresh and push Repo.")
+os.system("pause")
