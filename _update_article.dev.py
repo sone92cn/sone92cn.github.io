0f4c98c3b28a7141748483b9c643e5d8c4c5e656
sone92cn <554055459@qq.com>
2018-03-24 15:20:14 +0800

    v1.01

--- _update_article.dev.py
+++ _update_article.dev.py
@@ -8,20 +8,6 @@ Created on Tue Feb 27 11:44:43 2018
 import os, markdown
 from PathTool import createTreeAsPath, createDirAsTree
 
-root = os.path.split(os.path.realpath(__file__))[0]
-path = os.path.join(root, "article_md")
-
-tree = createTreeAsPath(path, scanSubFolder=True, treeMode=True, relativePath=True, forFile=False)
-
-if len(tree):
-	if createDirAsTree(os.path.join(root, "article_head"), tree) and createDirAsTree(os.path.join(root, "article_html"), tree):
-		print("Succeed to copy Dirs.")
-	else:
-		print("Fail to copy Dirs.")
-else:
-	print("No Dirs.")
-
-"""
 def findSubStr(substr, totalstr, i):
     count = 0
     index = -1
@@ -31,16 +17,73 @@ def findSubStr(substr, totalstr, i):
             break;
         else:
             count += 1
-            if count >= i:
-                break
+        if count >= i:
+            break
     return index
+
+if __name__ == "__main__":
+    root = os.path.split(os.path.realpath(__file__))[0]
+    path = os.path.join(root, "article_md")
+
+    tree = createTreeAsPath(path, scanSubFolder=True, treeMode=True, relativePath=True, forFile=False)
+    if len(tree):
+        if createDirAsTree(os.path.join(root, "article_html"), tree):
+            print("Succeed to copy Dirs.")
+        else:
+            print("Fail to copy Dirs.")
+    else:
+        print("No Sub Dirs.")
+
+    succ_c, fail_c, succ_d, fail_d = 0, 0, 0, 0
+    path = os.path.join(root, "article_md")
+
+    print("MD FILE Path :", path)
+    files = createTreeAsPath(path, fileRegular=r'^.+\.md$', scanSubFolder=True, relativePath=True)
+    for mfile in files:
+        fname = os.path.splitext(mfile)[0]
+        mfile = os.path.join("article_md", mfile)
+        tfile = os.path.join(os.path.join(root, "article_html"), fname + ".html")
+
+
+        if os.path.isfile(tfile):
+            print(tfile, os.path.isfile(tfile))
+            if os.path.getmtime(tfile) > os.path.getmtime(mfile):
+                continue
+
+        input_file = open(mfile, mode="r", encoding="utf-8")
+        input_text = input_file.read()
+        input_file.close()
+
+        try:
+            output_file = open(tfile, "w", encoding="utf-8")
+        except:
+            fail_c += 1
+            print("Fail to translate %s." % file)
+        else:
+            output_file.write("<article>")
+            output_file.write(markdown.markdown(input_text))
+            output_file.write("</article>")
+            output_file.close()
+            succ_c += 1
+            print("Succeed to transate %s." % mfile)
+
+
+
+
+
+
+
+"""
+index = findSubStr("\n", input_text, 10)
+        if index == -1:
+            head = text
+        else:
+            head = text[:index] + "\n..."
         
 
-succ_c, fail_c, succ_d, fail_d = 0, 0, 0, 0
+
 root = os.path.split(os.path.realpath(__file__))[0]
-path = os.path.join(root, "article_md")
-print("MD Path :", path)
-files = createTreeAsPath(path, fileRegular=r'^.+\.md$', scanSubFolder=False)
+
 
 for mfile in files:
     path, file = os.path.split(mfile)
@@ -107,5 +150,5 @@ print("<Update> succeed: %s files, Fail: %s files" % (succ_c, fail_c))
 print("<Delete> succeed: %s files, Fail: %s files" % (succ_d, fail_d))
 """
 
-os.system("pause")
+#os.system("pause")
     
