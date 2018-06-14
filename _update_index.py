79d1f71a957b88a6bc4416c22d2854ecc7558ecb
sone92cn <554055459@qq.com>
2018-06-14 14:13:55 +0800

    v1.03

--- _update_index.py
+++ _update_index.py
@@ -2,9 +2,7 @@ import os, pickle
 from PathTool import createTreeAsPath
 from jinja2 import FileSystemLoader, Environment
 
-path = os.path.split(os.path.realpath(__file__))[0]
-
-def RenderTemplate(menu, recents):
+def RenderTemplate(menu, recents, path):
     try:
         TemplateLoader = FileSystemLoader(searchpath=os.path.join(path, "template"), encoding='utf-8')
         TemplateEnv = Environment(loader=TemplateLoader)
@@ -13,22 +11,22 @@ def RenderTemplate(menu, recents):
         return False
     else:
         html = template.render(title="Felix's Page", menu=menu, recents=recents)
-        wfile = open(outputfile, "w", encoding="utf-8")
-        wfile.write(html)
-        wfile.close()
+        with open(os.path.join(path, "index.html"), "w", encoding="utf-8") as wfile:
+            wfile.write(html)
         return True
-    
+  
 if __name__ == "__main__":
     
-    inputfile = os.path.join(path, "template\\index.html")
-    outputfile = os.path.join(path, "index.html")
-
-    print("Input:", inputfile)
-    print("Output", outputfile)
-
-    with open("articles.pkl", "rb") as handle:
+    try:
+        path = os.path.split(os.path.realpath(__file__))[0]
+    except:
+        path = r'E:\Projects\Git\GitWeb'
+        print('Use static path')
+    
+    
+    with open(os.path.join(path, "articles.pkl"), "rb") as handle:
         articles = pickle.load(handle)
-
+    
     keys = list(articles.keys())[:6]
     recents = {key:articles[key] for key in keys}
 
@@ -44,9 +42,9 @@ if __name__ == "__main__":
         for title in titles:
             child[title[3:]] = os.path.join(os.path.join(os.path.join("article_html", head), title), "menu.htm").replace("\\", "/")
 
-    if RenderTemplate(menu=menu, recents=recents):
+    if RenderTemplate(menu=menu, recents=recents, path=path):
         print("Succeed to update index page.")
     else:
         print("Fail to update index page.")
-
+    
 os.system("pause")
